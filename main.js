8b7109ae25d9e84b3a1b9e09246dd477fad8a9f8
Bruno <Bruno@teste.com>
2025-01-26 16:47:04 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -51,9 +51,8 @@ function atualizaTabela(){
 function atualizaMediaFinal(){
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.tofixed(2);
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
-
 }
 
     function calculaMediaFinal(){
